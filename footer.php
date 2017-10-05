06290e654f21aa07268c2b9a71e47eb30a40f0e5
growoffs <f.bongo@growoffshore.com>
2017-10-05 14:18:30 +0800

    init push

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package CAP_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cap-theme' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'cap-theme' ), 'WordPress' );
+			?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cap-theme' ), 'cap-theme', '<a href="http://growoffshore.com">growoffshore</a>' );
+			?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Home page
+ *
+ */
+
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+			echo 'home';
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
+?>
