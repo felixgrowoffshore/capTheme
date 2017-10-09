608f3b26f1187c14cd420a8c7367bd1c102a7325
growoffs <f.bongo@growoffshore.com>
2017-10-09 18:41:16 +0800

    add search result template

---
+++ template-parts/search-result.php
@@ -0,0 +1,17 @@
+<div class="container results">
+  <div class="row">
+    <div class="col-md-12">
+      <h3>Results</h3>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <ul>
+        <li><a href="#">Aberdeenshire Museums Service <span>CaP</span></a></li>
+        <li><a href="#">Avon and Somerset Constabulary History & Heritage Group</a></li>
+        <li><a href="#">Barnsley Archives and Local Studies <span>CaP</span></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
