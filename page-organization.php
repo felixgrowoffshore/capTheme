604f150fa9dcc4fa6106ebb3392d836051700ca2
growoffs <f.bongo@growoffshore.com>
2017-10-10 19:37:18 +0800

    organisation structure

--- page-organization.php
+++ page-organization.php
@@ -9,13 +9,16 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main ">
+			<div class="home-feat-wrap">
+				<?php get_template_part( 'template-parts/_get_featured' ); ?>
+			</div>
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
 						<?php
 						while ( have_posts() ) : the_post();
 
-							get_template_part( 'template-parts/_search_collection' );
+							get_template_part( 'template-parts/organisation', 'page' );
 							// If comments are open or we have at least one comment, load up the comment template.
 							if ( comments_open() || get_comments_number() ) :
 								comments_template();
--- template-parts/organisation-page.php
+++ template-parts/organisation-page.php
@@ -6,7 +6,7 @@
 			<h1>Tetbury Museum and Courtroom</h1>
 		</div>
 		<div class="col-md-4 cap">
-			<p>CaP Member</p>
+			<p class="bullet_cap">CaP Member<span>CaP</span></p>
 		</div>
 	</div>
 
@@ -16,9 +16,9 @@
 				<p>Organisation: Museum</p>
 				<p>Collection: Police, Court</p>
 				<p>Dates: 1700 - 1950</p>
-				
+
 				<p>A number of interesting displays of photographs and equipment and show the history of policing. Our resident sergeant is always on duty in the station watching over the prisoners</p>
-				
+
 				<p> <i class="fa fa-map-marker"></i> 63 Long St, Tetbury GL8 8AA</p>
 				<p> <i class="fa fa-envelope"></i> Contact: John Silvester</p>
 			</div>
@@ -28,13 +28,13 @@
 			<div class="col-md-8 information">
 				<p class="bold">Any publications or other relevant material about the collection / history of the organisation?</p>
 				<p class="spacing">Yes</p>
-				
+
 				<p class="bold">Historic Buildings</p>
 				<p class="spacing">Yes - we have an old courtroom</p>
-				
+
 				<p class="bold">Who can access the collection?</p>
 				<p class="spacing">Registered users</p>
-			
+
 				<p class="bold">How big is the collection?</p>
 				<ul class="col-md-2 list">
 					<li>Documents</li>
@@ -53,11 +53,11 @@
 			<div class="col-md-4 image-2">
 				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
 			</div>
-	
+
 
 
 		</div>
-		
+
 	</div>
-	
+
 </div>
