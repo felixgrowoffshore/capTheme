28db58b9a1a8d5128dbde2c623ee76a98c683736
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-10 17:19:28 +0800

    search collection/organization template

--- template-parts/_search_collection.php
+++ template-parts/_search_collection.php
@@ -2,30 +2,59 @@
 
 <div class="container collection-content">
 	<div class="row">
-		<div class="col-lg-8">
+		<div class="col-md-8">
 			<h1>Tetbury Museum and Courtroom</h1>
 		</div>
-		<div class="col-lg-4 cap">
+		<div class="col-md-4 cap">
 			<p>CaP Member</p>
 		</div>
 	</div>
 
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-8 infos">
+			<div class="col-md-8 infos">
 				<p>Organisation: Museum</p>
 				<p>Collection: Police, Court</p>
 				<p>Dates: 1700 - 1950</p>
-				<br>
-				<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
-				<br>
-				<p> <i class="fa fa-car"></i> hahaha</p>
+				
+				<p>A number of interesting displays of photographs and equipment and show the history of policing. Our resident sergeant is always on duty in the station watching over the prisoners</p>
+				
+				<p> <i class="fa fa-map-marker"></i> 63 Long St, Tetbury GL8 8AA</p>
+				<p> <i class="fa fa-envelope"></i> Contact: John Silvester</p>
 			</div>
-			<div class="col-lg-4 collection-image">
-				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
-				<br>
+			<div class="col-md-4 collection-image">
 				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
 			</div>
+			<div class="col-md-8 information">
+				<p class="bold">Any publications or other relevant material about the collection / history of the organisation?</p>
+				<p class="spacing">Yes</p>
+				
+				<p class="bold">Historic Buildings</p>
+				<p class="spacing">Yes - we have an old courtroom</p>
+				
+				<p class="bold">Who can access the collection?</p>
+				<p class="spacing">Registered users</p>
+			
+				<p class="bold">How big is the collection?</p>
+				<ul class="col-md-2 list">
+					<li>Documents</li>
+					<li>Photographs</li>
+					<li>Objects</li>
+				</ul>
+				<ul class="col-md-2 list">
+					<li>11-100</li>
+					<li>101-505</li>
+					<li>Unknown</li>
+				</ul>
+
+
+
+			</div>
+			<div class="col-md-4 image-2">
+				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
+			</div>
+	
+
 
 		</div>
 		
