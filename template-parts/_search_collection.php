94e7902cf87979f5f30d38d02472cef9888db0f5
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-10 15:31:50 +0800

    3rd revision search collection/organization

--- template-parts/_search_collection.php
+++ template-parts/_search_collection.php
@@ -1,25 +1,32 @@
-<div class="container">
+<!--organization-->
+
+<div class="container collection-content">
 	<div class="row">
-		<div class="col-lg-8 offset-2">
+		<div class="col-lg-8">
 			<h1>Tetbury Museum and Courtroom</h1>
 		</div>
-		<div class="col-lg-2 cap">
+		<div class="col-lg-4 cap">
 			<p>CaP Member</p>
 		</div>
 	</div>
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-7">
+			<div class="col-lg-8 infos">
 				<p>Organisation: Museum</p>
 				<p>Collection: Police, Court</p>
 				<p>Dates: 1700 - 1950</p>
 				<br>
-				<p>hahahahahahaha</p>
+				<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+				<br>
+				<p> <i class="fa fa-car"></i> hahaha</p>
 			</div>
-			<div class="col-md-5">
-				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_4.png">
+			<div class="col-lg-4 collection-image">
+				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
+				<br>
+				<img src="http://localhost:84/WP/wp-content/uploads/2017/10/Screenshot_5.png">
 			</div>
+
 		</div>
 		
 	</div>
