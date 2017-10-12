ddd9df27738417fda94d3c2820ace9f998046501
growoffs <f.bongo@growoffshore.com>
2017-10-12 14:04:19 +0800

    home styles

--- template-parts/_get_children.php
+++ template-parts/_get_children.php
@@ -10,10 +10,15 @@ if($children) : ?>
 <div class="container"><?php
 // var_dump($children);
   foreach ($children as $key => $page) { ?>
+    <?php
+    $alternative_title = get_field('alternative_title',$page->ID);
+    ?>
     <div class="row home-child">
       <div class="col-sm-12 col-md-8">
-        <h2><?php echo $page->post_title; ?></h2>
+        <h2><?php echo $alternative_title; ?></h2>
         <p><?php echo $page->post_content; ?></p>
+
+        <p class="child-btn"><a class="btn btn-primary" href="<?= get_permalink($page->ID); ?>"><?= $page->post_title ?></a></p>
       </div>
     </div>
   <?php } ?>
