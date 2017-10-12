3109eee01fdf4f0dd7a8e2ad8c41d92ec3b3e82e
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-12 14:59:21 +0800

    adding scss file

--- template-parts/news-listing.php
+++ template-parts/news-listing.php
@@ -34,7 +34,7 @@ if($news_events) : ?>
           <div class="col-md-8 col-sm-12 col-lg-9">
             <div class="news-info">
               <h3><?php echo $news->post_title ?></h3>
-              <p><?php echo date('jS M Y',$news->post_date) ?></p>
+              <p class="bold"><?php echo date('jS M Y',$news->post_date) ?></p>
               <?php
               $post_content = get_post($news->ID);
               $content = $post_content->post_content;
